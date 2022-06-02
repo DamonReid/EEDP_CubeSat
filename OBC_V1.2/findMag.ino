8806217bf7cca681497197496b17b024ef5618a6
Emma Marsden <z5265510@unsw.adfa.edu.au>
2022-06-02 10:52:06 +1000

    Change to variable names

--- OBC_V1.2/findMag.ino
+++ OBC_V1.2/findMag.ino
@@ -1,13 +1,11 @@
 String readString; //main captured String
-String angle; //data String
-String fuel;
-String speed1;
-String altidude;
+String x_value;
+String y_value;
+String z_value;
 
-int ind1; // , locations
+int ind1; // locations
 int ind2;
 int ind3;
-
 
 void setup() {
     Serial.begin(9600);
@@ -19,24 +17,25 @@ void loop() {
         char c = Serial.read();  //gets one byte from serial buffer
         if (c == '\n') {
             Serial.println();
-            Serial.print("Captured String is : ");
+            Serial.print("Captured String is: ");
             Serial.println(readString); //prints string to serial port out
 
             ind1 = readString.indexOf('x:');  //finds location of first ,
-            x_value = readString.substring(0, ind1);   //captures first data String
             ind2 = readString.indexOf('y:', ind1+1 );   //finds location of second ,
-            y_value = readString.substring(ind1+1, ind2+1);   //captures second data String
             ind3 = readString.indexOf('z:', ind2+1 );
-            z_value = readString.substring(ind2+1, ind3+1);
+            ind4 = readString.indexOf('c', ind3+1);
 
-            Serial.print("angle = ");
-            Serial.println(angle);
-            Serial.print("fuel = ");
-            Serial.println(fuel);
-            Serial.print("speed = ");
-            Serial.println(speed1);
-            Serial.print("altidude = ");
-            Serial.println(altidude);
+            x_value = readString.substring(ind1, ind2-1);   //captures first data String
+            y_value = readString.substring(ind2, ind3-1);   //captures second data String
+            z_value = readString.substring(ind3, ind4-1);
+
+            Serial.print("X = ");
+            Serial.println(x_value);
+            Serial.print("Y = ");
+            Serial.println(y_value);
+            Serial.print("Z = ");
+            Serial.println(z_value);
+
             Serial.println();
             Serial.println();
 
