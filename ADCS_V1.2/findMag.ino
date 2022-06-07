01c73f54e06456b429c4fa323f4f7acda715daa5
emmamarsden <100646291+emmamarsden@users.noreply.github.com>
2022-06-07 15:42:21 +1000

    Update findMag.ino

--- ADCS_V1.2/findMag.ino
+++ ADCS_V1.2/findMag.ino
@@ -1,52 +1,129 @@
-String readString; //main captured String
-String x_value;
-String y_value;
-String z_value;
-
-int ind1; // locations
-int ind2;
-int ind3;
+char serialIn[100];
+float xVal;
+float yVal;
+float zVal;
 
 void setup() {
-    Serial.begin(9600);
-    Serial.println("Serial Delimit Test"); // so I can keep track of what is loaded
+  Serial.begin(9600);
+  Serial.println("Serial Delimit Test");
 }
 
 void loop() {
-    if (Serial.available())  {
-        char c = Serial.read();  //gets one byte from serial buffer
-        if (c == '\n') {
-            Serial.println();
-            Serial.print("Captured String is: ");
-            Serial.println(readString); //prints string to serial port out
+  while (!Serial.available()) {}
+  int input = Serial.readBytesUntil('\n', serialIn, sizeof(serialIn) - 1);
 
-            ind1 = readString.indexOf('x:');  //finds location of first ,
-            ind2 = readString.indexOf('y:', ind1+1 );   //finds location of second ,
-            ind3 = readString.indexOf('z:', ind2+1 );
-            ind4 = readString.indexOf('c', ind3+1);
+  Serial.println();
+  Serial.print("Captured String is ");
+  Serial.println(serialIn); //prints string to serial port out
 
-            x_value = readString.substring(ind1, ind2-1);   //captures first data String
-            y_value = readString.substring(ind2, ind3-1);   //captures second data String
-            z_value = readString.substring(ind3, ind4-1);
+  char *in1 = strtok(serialIn, ",");
+  String i1 = String(in1);
+  Serial.println(in1);
 
-            Serial.print("X = ");
-            Serial.println(x_value);
-            Serial.print("Y = ");
-            Serial.println(y_value);
-            Serial.print("Z = ");
-            Serial.println(z_value);
+  char *in2 = strtok(NULL,  ",");
+  String i2 = String(in2);
+  Serial.println(in2);
 
-            Serial.println();
-            Serial.println();
+  char *in3 = strtok(NULL,  ",");
+  String i3 = String(in3);
+  Serial.println(in3);
 
-            readString=""; //clears variable for new input
-            angle="";
-            fuel="";
-            speed1="";
-            altidude="";
-        }
-        else {
-            readString += c; //makes the string readString
-        }
-    }
+  switch (pmagAxis) {
+    case 'x':
+      if (i1.charAt(0) == 'x') {
+        char *xLoc = strtok(in1, ": ");
+        char *xLoc1 = strtok(NULL, " ");
+        String xRoot =  String(xLoc1);
+        xRoot.substring(4, sizeof(xRoot) - 1);
+
+        Serial.println(xRoot);
+        xVal = xRoot.toFloat();
+      }
+      else if (i2.charAt(0) == 'x' || i2.charAt(1) == 'x') {
+        char *xLoc = strtok(in2, ": ");
+        char *xLoc1 = strtok(NULL, " ");
+        String xRoot =  String(xLoc1);
+        xRoot.substring(4, sizeof(xRoot) - 1);
+
+        Serial.println(xRoot);
+        xVal = xRoot.toFloat();
+      }
+      else if (i3.charAt(0) == 'x' || i3.charAt(1) == 'x') {
+        char *xLoc = strtok(in3, ": ");
+        char *xLoc1 = strtok(NULL, " ");
+        String xRoot =  String(xLoc1);
+        xRoot.substring(4, sizeof(xRoot) - 1);
+
+        Serial.println(xRoot);
+        xVal = xRoot.toFloat();
+      }
+
+      return xVal;
+
+    case 'y':
+      if (i1.charAt(0) == 'y') {
+        char *yLoc = strtok(in1, ": ");
+        char *yLoc1 = strtok(NULL, " ");
+        String yRoot =  String(yLoc1);
+        yRoot.substring(4, sizeof(yRoot) - 1);
+
+        Serial.println(yRoot);
+        yVal = yRoot.toFloat();
+      }
+      else if (i2.charAt(0) == 'y' || i2.charAt(1) == 'y') {
+        char *yLoc = strtok(in2, ": ");
+        char *yLoc1 = strtok(NULL, " ");
+        String yRoot =  String(yLoc1);
+        yRoot.substring(4, sizeof(yRoot) - 1);
+
+        Serial.println(yRoot);
+        yVal = yRoot.toFloat();
+      }
+      else if (i3.charAt(0) == 'y' || i3.charAt(1) == 'y') {
+        char *yLoc = strtok(in3, ": ");
+        char *yLoc1 = strtok(NULL, " ");
+        String yRoot =  String(yLoc1);
+        yRoot.substring(4, sizeof(yRoot) - 1);
+
+        Serial.println(yRoot);
+        yVal = yRoot.toFloat();
+      }
+
+      return yVal;
+
+    case 'z':
+      if (i1.charAt(0) == 'z') {
+        char *zLoc = strtok(in1, ": ");
+        char *zLoc1 = strtok(NULL, " ");
+        String zRoot =  String(zLoc1);
+        zRoot.substring(4, sizeof(zRoot) - 1);
+
+        Serial.println(zRoot);
+        zVal = zRoot.toFloat();
+      }
+      else if (i2.charAt(0) == 'z' || i2.charAt(1) == 'z') {
+        char *zLoc = strtok(in2, ": ");
+        char *zLoc1 = strtok(NULL, " ");
+        String zRoot =  String(zLoc1);
+        zRoot.substring(4, sizeof(zRoot) - 1);
+
+        Serial.println(zRoot);
+        zVal = zRoot.toFloat();
+      }
+      else if (i3.charAt(0) == 'x' || i3.charAt(1) == 'z') {
+        char *zLoc = strtok(in3, ": ");
+        char *zLoc1 = strtok(NULL, " ");
+        String zRoot =  String(zLoc1);
+        zRoot.substring(4, sizeof(zRoot) - 1);
+
+        Serial.println(zRoot);
+        zVal = zRoot.toFloat();
+      }
+
+      return zVal;
+
+    default:
+      return "fail, 1";
+
+  }
 }
