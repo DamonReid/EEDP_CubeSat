922f1a4b7781f054c7a5d2e784d2710272436d47
dan-piv <dan.pivetta1@gmail.com>
2022-06-08 13:20:16 +1000

    Signed-off-by: dan-piv <dan.pivetta1@gmail.com>

--- OBC_V1.2/b-dot/b-dot.ino
+++ OBC_V1.2/b-dot/b-dot.ino
@@ -1,9 +1,22 @@
-void setup() {
-  // put your setup code here, to run once:
 
+float mag[3];
+float maghist[3];
+float curr[3];
+float m[3];
+float kw;
+
+void bDot(){
+  float k = (-kw/abs(magnetometer)^2)+(mag[1]^2+mag[2]^2+mag[3]^2);
+  m = (mag-maghist*k;
+  
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
 
-}
+/*m = (b(n)-b(n-1)*k)
+ * 
+ * k = -kw/norm(b(n))^2 (bx^2+by^2+bz^2)
+ * 
+ * kw = .4e-05
+ * 
+ * 
+ */
