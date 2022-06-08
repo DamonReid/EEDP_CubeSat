ed42336f6ade456ae71e1ee0e9fa5f6211d9aea2
Emma Marsden <z5265510@unsw.adfa.edu.au>
2022-06-08 15:12:43 +1000

    dbot. generate getTemp

---
+++ ADCS_V1.2/getTemp/getTemp.ino
@@ -0,0 +1,16 @@
+#include <math.h>
+
+void setup() {
+    Serial.begin(9600);
+    tempPin = A1;
+}
+void loop() {
+    double sensorIn, value, value1, tempVal;
+
+    sensorIn = analogRead(tempPin);
+    value = (sensorIn/1023)*5;
+    value1 = (5-value)/value*4700;
+    tempVal = 1/(log(value1/10000)/3950 + 1/(25+273.15)) - 273.15;
+}
+
+
